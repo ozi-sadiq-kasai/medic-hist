a9e1d55e6c5146ce52e46b3476aa11dec2d6cd09
ozi <ozisadiqkasai@gmail.com>
2023-08-18 00:47:00 +0100

    built sign in page

--- medhist/src/component/Signup.js
+++ medhist/src/component/Signup.js
@@ -1,5 +1,6 @@
 import { auth,googleProvider } from "../firebase"
 import { createUserWithEmailAndPassword, signInWithPopup,signOut} from "firebase/auth"
+import { FcGoogle } from 'react-icons/fc';
 import { useState } from "react"
 
 const Signup = () => {
@@ -42,14 +43,21 @@ const Signup = () => {
  }
 
   return (
-   <div>
-    <h1>Sign In</h1>
-    <label htmlFor="email">Email<input type="email" name="email" onChange={handleChangeEmail}/></label>
-    <label htmlFor="password">Password<input type="password" name="password" onChange={handleChangePassword}/></label>
-    <button onClick={signin}>Sign In</button> 
-    <button onClick={signinWithGoogle}>Sign In With Google</button> 
-    <button onClick={logout}>Sign Out</button> 
-   </div>
+<main className="w-screen h-screen flex justify-center items-center">
+  <div className="border border-gray-100 w-4/5 p-4 flex flex-col items-center py-28 shadow-lg sm:w-screen lg:w-3/5">
+    <h1 className="font-roboto font-bold text-3xl p-1 text-green-300">Sign In</h1>
+    <input className="outline-0 mt-3 mb-4 border-b-2 border-gray-100 w-2/5" type="email" placeholder="Email" onChange={handleChangeEmail} />
+    <input className="outline-0 mt-3 mb-4 border-b-2 border-gray-100 w-2/5" type="password" placeholder="Password" onChange={handleChangePassword} />
+
+    <button className="outline-0 text-slate-300 py-1 px-14 border mb-7 mt-5"onClick={signin}>Sign In with Email</button>
+    <div className="outline-0 flex items-center py-1 px-10 border text-slate-300">
+     <FcGoogle /><button onClick={signinWithGoogle}>Sign In With Google</button>
+    </div>
+   
+
+    <button className="outline-0 py-1 px-10 bg-red-700 mt-8 text-slate-300 rounded-sm"onClick={logout}>Sign Out</button>
+  </div>
+</main>
   )
 }
 export default Signup
--- medhist/tailwind.config.js
+++ medhist/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      screens: {
+      'sm':  {'min': '300px', 'max': '450px'},
+    }
+    }
   },
   plugins: [],
 }
